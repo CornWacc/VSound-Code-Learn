8e3c84be14462b72a06f76423b63b4f40babfc62
Corn <791621554@qq.com>
2019-08-15 21:57:01 +0800

    新增主页

---
+++ vsound_code_Learn_web/src/components/main.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-container class="container">
+      <el-header class="header">
+      </el-header>
+      <el-main class="main">
+      </el-main>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        name: "main"
+    }
+</script>
+
+<style scoped>
+.container{
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(#F8F8FF,#BBFFFF);
+}
+</style>
--- vsound_code_Learn_web/src/router/index.js
+++ vsound_code_Learn_web/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index'
-
+import main from '@/components/main'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: index
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component : main
     }
   ]
 })
