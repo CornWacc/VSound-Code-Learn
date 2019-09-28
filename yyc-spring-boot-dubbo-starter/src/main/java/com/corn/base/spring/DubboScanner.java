d2e4dbf68563df77b99d5d64471aa13303f26fc0
Corn <791621554@qq.com>
2019-09-28 17:42:07 +0800

    修改

--- yyc-spring-boot-dubbo-starter/src/main/java/com/corn/base/spring/DubboScanner.java
+++ yyc-spring-boot-dubbo-starter/src/main/java/com/corn/base/spring/DubboScanner.java
@@ -1,11 +1,15 @@
 package com.corn.base.spring;
 
+import org.apache.dubbo.config.spring.context.annotation.DubboClassPathBeanDefinitionScanner;
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScanRegistrar;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@EnableDubbo(scanBasePackages = "${dubbo.scan.package}")
+@EnableDubbo()
 @PropertySource("classpath:application.properties")
 public class DubboScanner {
+
 }
