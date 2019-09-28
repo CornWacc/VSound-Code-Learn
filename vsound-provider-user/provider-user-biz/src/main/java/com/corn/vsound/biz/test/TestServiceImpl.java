0bd7d40f695c75f3f4d1cba84ba5a362a78bb8e3
Corn <791621554@qq.com>
2019-09-28 17:41:41 +0800

    新增dubbo配置

--- vsound-provider-user/provider-user-biz/src/main/java/com/corn/vsound/biz/test/TestServiceImpl.java
+++ vsound-provider-user/provider-user-biz/src/main/java/com/corn/vsound/biz/test/TestServiceImpl.java
@@ -3,10 +3,11 @@ package com.corn.vsound.biz.test;
 
 import com.alibaba.dubbo.config.annotation.Service;
 
-@Service
+@Service(version = "1.0")
 public class TestServiceImpl implements TestService {
 
     public String say() {
+        System.out.println("hello");
         return "hello";
     }
 
--- vsound-provider-user/provider-user-run/src/main/java/com/corn/vsound/Main.java
+++ vsound-provider-user/provider-user-run/src/main/java/com/corn/vsound/Main.java
@@ -1,16 +1,22 @@
 package com.corn.vsound;
 
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import com.corn.boot.annotations.Runner;
 import com.corn.boot.base.RunMain;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Runner(port = "9099",active = "mac")
+@Runner(port = "9099",active = "window")
 @NacosPropertySource(dataId = "example",autoRefreshed = true)
 @RestController
+@Configuration
+@EnableDubbo(scanBasePackages = "com.corn.vsound.biz")
+@PropertySource("classpath:application-window.properties")
 public class Main {
 
     public static void main(String[] args) {
