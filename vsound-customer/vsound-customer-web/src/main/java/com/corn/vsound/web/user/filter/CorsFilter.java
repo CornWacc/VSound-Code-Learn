e50e05d43a33f728e9287a44081d424551bf7ecd
Yyc <791621554@qq.com>
2019-11-29 16:50:57 +0800

    修改

---
+++ vsound-customer/vsound-customer-web/src/main/java/com/corn/vsound/web/user/filter/CorsFilter.java
@@ -0,0 +1,59 @@
+package com.corn.vsound.web.user.filter;
+
+import com.corn.boot.base.HttpBase;
+import com.corn.boot.base.LogClientBase;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author yyc
+ * @apiNote 跨域过滤器
+ * */
+public class CorsFilter extends LogClientBase implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("********** 跨域过滤器初始化 **********");
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String method = request.getMethod();
+        log.info("接口请求:{},方式:{}",request.getRequestURI(),request.getMethod());
+
+        response.setHeader("Access-Control-Expose-Headers", "isign,app,x-requested-with,content-type,No-Cache");
+        //请求方法
+        response.setHeader("Access-Control-Request-Method", "GET,POST,PUT,DELETE,OPTIONS");
+        //请求头
+        response.setHeader("Access-Control-Request-Headers", "app,x-requested-with,content-type,No-Cache");
+        //同意的方法
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        //同意的请求头
+        response.setHeader("Access-Control-Allow-Headers", "app,x-requested-with,content-type,No-Cache");
+
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        response.setHeader("Access-Control-Allow-Origin","*");
+
+        response.setCharacterEncoding(HttpBase.HTTP_CHARSET);
+
+        if(HttpBase.HTTP_REQUEST_METHOD_OPTION.equals(method)){
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().write("cross pass ok !");
+            return;
+        }
+
+        filterChain.doFilter(request,response);
+
+    }
+
+    public void destroy() {
+        log.info("********** 跨域过滤器销毁 **********");
+    }
+
+}
---
+++ vsound-customer/vsound-customer-web/src/main/java/com/corn/vsound/web/user/filter/FilterBeanRegistryConfiguration.java
@@ -0,0 +1,23 @@
+package com.corn.vsound.web.user.filter;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yyc
+ * @apiNote 过滤器初始化注入
+ * */
+@Configuration
+public class FilterBeanRegistryConfiguration {
+
+    @Bean("corsFilter")
+    public FilterRegistrationBean corsFilter(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new CorsFilter());
+        registrationBean.setOrder(1);
+        registrationBean.addUrlPatterns("*");
+        return registrationBean;
+    }
+}
