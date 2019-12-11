b7909963523f84ae32781b76109fb7f9e2de157d
Yyc <791621554@qq.com>
2019-12-11 15:21:26 +0800

    修改

---
+++ vsound-provider/vsound-facade/src/main/java/com/corn/vsound/facade/code/result/CodeOutSideUrlCUDResult.java
@@ -0,0 +1,12 @@
+package com.corn.vsound.facade.code.result;
+
+import com.corn.boot.base.pojobase.BaseRes;
+
+/**
+ * @author yyc
+ * @apiNote
+ * @createTime 2019/12/11
+ */
+public class CodeOutSideUrlCUDResult extends BaseRes {
+    private static final long serialVersionUID = 8034311194791399431L;
+}
