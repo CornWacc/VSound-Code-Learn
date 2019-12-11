ade3227dd626e7bb8b230a5174b36f21b4ac0ceb
Yyc <791621554@qq.com>
2019-12-11 15:20:52 +0800

    修改

---
+++ vsound-provider/vsound-facade/src/main/java/com/corn/vsound/facade/code/order/CodeOutSideUrlCUDOrder.java
@@ -0,0 +1,12 @@
+package com.corn.vsound.facade.code.order;
+
+import com.corn.boot.base.strategy.CUDOrder;
+
+/**
+ * @author yyc
+ * @apiNote
+ * @createTime 2019/12/11
+ */
+public class CodeOutSideUrlCUDOrder extends CUDOrder {
+    private static final long serialVersionUID = 29646677700876356L;
+}
