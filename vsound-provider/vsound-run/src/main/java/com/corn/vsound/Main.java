0658403f8f820dd0f18539a98dff297aa5efa827
Yyc <791621554@qq.com>
2019-12-13 15:16:29 +0800

    增加DockerFile，配置多环境

--- vsound-provider/vsound-run/src/main/java/com/corn/vsound/Main.java
+++ vsound-provider/vsound-run/src/main/java/com/corn/vsound/Main.java
@@ -6,7 +6,7 @@ import com.corn.boot.base.RunMain;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ImportResource;
 
-@Runner(port = "9053")
+@Runner(port = "9527",active = "test")
 @ImportResource("classpath:dubbo-admin.xml")
 @MapperScan("com.corn.vsound.dao.mapper")
 public class Main {
