4096e0a1e6893f4c4cd8b7a5bfa4e52955ef402d
Yyc <791621554@qq.com>
2019-12-28 15:52:58 +0800

    修改

--- vsound-provider/vsound-dao/src/main/java/com/corn/vsound/dao/mapper/CodeMethodMapper.java
+++ vsound-provider/vsound-dao/src/main/java/com/corn/vsound/dao/mapper/CodeMethodMapper.java
@@ -1,15 +1,13 @@
 package com.corn.vsound.dao.mapper;
 
 import com.corn.vsound.dao.entity.CodeMethod;
-import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface CodeMethodMapper {
-    int deleteByPrimaryKey(String methodId);
+
+    int deleteByPrimaryKey(String id);
 
     int insert(CodeMethod record);
 
@@ -21,12 +19,18 @@ public interface CodeMethodMapper {
 
     int updateByPrimaryKey(CodeMethod record);
 
-    @Select("select * from code_method where method_id = #{methodId}")
-    @ResultMap("BaseResultMap")
-    CodeMethod findCodeMethodByMethodId(@Param("methodId")String methodId);
-
+    /**
+     * 根据所属源码id查询方法列表
+     * */
     List<CodeMethod> findCodeMethodListByCodeId(@Param("codeId")String codeId);
 
-    int deleteCodeMethodsByCodeIds(@Param("codeIds")List<String> codeIds);
+    /**
+     * 根据方法id查询方法
+     * */
+    CodeMethod findCodeMethodByMethodId(@Param("methodId")String methodId);
 
+    /**
+     * 根据源码Ids删除对应的方法
+     * */
+    int deleteCodeMethodsByCodeIds(@Param("codeIds")List<String> codeIds);
 }
