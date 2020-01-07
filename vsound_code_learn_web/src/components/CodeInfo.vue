8b5b46409f4dd457e7c392a13e7807694121d76e
Yyc <791621554@qq.com>
2020-01-07 17:29:11 +0800

    修改页面

--- vsound_code_learn_web/src/components/CodeInfo.vue
+++ vsound_code_learn_web/src/components/CodeInfo.vue
@@ -1,15 +1,205 @@
 <template>
-  <el-container>
+  <el-container class="containers">
+    <el-header class="header">
+      <el-row :gutter="24" class="header_row">
+        <el-col :span="22" class="header_row_project_name">
+          <span>{{codeBase.codeName}}</span>
+        </el-col>
+        <el-col :span="1">
+          <el-button @click="toMain" type="primary" size="medium">返回主页</el-button>
+        </el-col>
+      </el-row>
+    </el-header>
+
+    <el-main class="main">
+      <div style="margin-bottom: 10px">
+        <p style="font-size: 20px;;margin-bottom: 4px">源码参数:</p>
+        <mu-button small color="#008B8B">新增</mu-button>
+      </div>
+
+      <el-table :data="codeBase.codeParameterInfos">
+        <el-table-column align="center" label="参数id" prop="parameterId"></el-table-column>
+        <el-table-column align="center" label="参数名称" prop="parameterName"></el-table-column>
+        <el-table-column align="center" label="参数类型" prop="parameterType"></el-table-column>
+        <el-table-column align="center" label="是否为常量" prop="isFinal"></el-table-column>
+        <el-table-column align="center" label="是否Autowire引入" prop="isAutowire"></el-table-column>
+        <el-table-column align="center" label="参数类型是否为接口" prop="isInterface"></el-table-column>
+        <el-table-column align="center" label="操作">
+          <template slot-scope="scope">
+            <mu-button icon color="primary">
+              <mu-icon value="edit"></mu-icon>
+            </mu-button>
+            <mu-button icon color="error">
+              <mu-icon value="delete"></mu-icon>
+            </mu-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <div style="margin-bottom: 10px">
+        <p style="font-size: 20px;margin-bottom: 4px">源码方法:</p>
+        <mu-button small color="#008B8B">新增</mu-button>
+      </div>
+
+      <el-table :data="codeBase.codeMethodInfoList">
+        <el-table-column align="center" label="方法名称" prop="methodName"></el-table-column>
+        <el-table-column align="center" label="方法返回参数" prop="methodResult"></el-table-column>
+        <el-table-column align="center" label="方法基础类型" prop="methodBaseType"></el-table-column>
+        <el-table-column align="center" label="是否常用" prop="methodCommonUse"></el-table-column>
+        <el-table-column align="center" label="是否为构造方法" prop="methodIsConstruct"></el-table-column>
+        <el-table-column align="center" label="是否重写" prop="methodIsOverwrite"></el-table-column>
+        <el-table-column align="center" label="方法作用域" prop="methodActionScope"></el-table-column>
+        <el-table-column align="center" label="操作">
+          <template slot-scope="scope">
+            <mu-button icon color="primary">
+              <mu-icon value="edit"></mu-icon>
+            </mu-button>
+            <mu-button icon color="error">
+              <mu-icon value="delete"></mu-icon>
+            </mu-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+
+      <div style="margin-bottom: 10px">
+        <p style="font-size: 20px;;margin-bottom: 4px">源码解析链接:</p>
+        <mu-button small color="#008B8B">新增</mu-button>
+      </div>
+
+      <el-table :data="codeBase.codeOutSideUrlInfos">
+        <el-table-column align="center" label="链接id" prop="urlId"></el-table-column>
+        <el-table-column align="center" label="链接地址" prop="urlPath">
+          <template slot-scope="scope">
+            <el-link type="primary" :href="scope.row.urlPath">{{scope.row.urlPath}}</el-link>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="链接备注" prop="urlRemark"></el-table-column>
+        <el-table-column align="center" label="操作">
+          <template slot-scope="scope">
+            <mu-button icon color="primary">
+              <mu-icon value="edit"></mu-icon>
+            </mu-button>
+            <mu-button icon color="error">
+              <mu-icon value="delete"></mu-icon>
+            </mu-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-main>
+
+    <el-dialog
+      :title="configurationInfo.dialog.title"
+      :visible.sync="configurationInfo.dialog.dialogIsShow"
+      width="30%"
+      :before-close="handleClose">
+
+      <el-form :model="cudCodeParameterForm" :label-position="configurationInfo.labelPosition"
+               ref="cudCodeParameterForm"
+               label-width="100">
+      </el-form>
+
+      <el-form :model="cudCodeMethodForm" :label-position="configurationInfo.labelPosition" ref="cudCodeMethodForm"
+               label-width="100">
+      </el-form>
+
+      <el-form :model="cudCodeOutSideUrlForm" :label-position="configurationInfo.labelPosition"
+               ref="cudCodeOutSideUrlForm"
+               label-width="100">
+      </el-form>
+
+
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="configurationInfo.dialog.dialogIsShow = false">取 消</el-button>
+    <el-button type="primary" @click="configurationInfo.dialog.dialogIsShow = false">确 定</el-button></span>
+    </el-dialog>
 
   </el-container>
 </template>
 
 <script>
-    export default {
-        name: "CodeInfo"
+  export default {
+    name: "CodeInfo",
+    data() {
+      return {
+        // 配置文件info
+        configurationInfo: {
+          dialog: {
+            dialogIsShow: true,
+            cudType: "",
+            title:"",
+            type:""
+          },
+          labelPosition: "left"
+        },
+        // 源码基础信息
+        codeBase: {
+          codeId: "",
+          codeName: "asd",
+          codeType: "",
+          codeMethodInfoList: [],
+          codeParameterInfos: [{parameterId: "123", parameterName: "123", parameterType: "CLASS", remark: "12312312"}],
+          codeOutSideUrlInfos: []
+        },
+        cudCodeParameterForm: {},
+        cudCodeMethodForm: {},
+        cudCodeOutSideUrlForm: {}
+      }
+    },
+    mounted() {
+
+      this.getCodeDetail();
+    },
+    methods: {
+
+      /**
+       * 源码详情
+       * */
+      getCodeDetail() {
+        this.$axios({
+          url: this.Globel.requestUrl + "/code/codeDetailQuery?codeId=" + this.$route.query.codeId
+        }).then(res => {
+          if (res.data.success) {
+            console.log(res)
+            this.codeBase = res.data.data;
+          } else {
+            this.$message.error(res.data.msg)
+          }
+        })
+      },
+
+      /**
+       * 返回主页
+       * */
+      toMain() {
+        this.$router.push({path: "/"});
+      },
+
+      /**
+       * 弹出框关闭回调
+       * */
+      handleClose() {
+
+      }
+
     }
+  }
 </script>
 
 <style scoped>
+  .header {
+    /*border-bottom: 1px solid aquamarine;*/
+  }
 
+  .header_row {
+    line-height: 60px;
+    height: 60px;
+  }
+
+  .header_row_project_name {
+    font-size: 30px;
+    font-family: "Kaiti SC";
+    font-weight: 400;
+    /*margin-left: 10px;*/
+  }
 </style>
--- vsound_code_learn_web/src/components/ProjectInfo.vue
+++ vsound_code_learn_web/src/components/ProjectInfo.vue
@@ -7,7 +7,7 @@
           <span>{{projectBase.projectName}}</span>
         </el-col>
         <el-col :span="1">
-          <el-button>返回主页</el-button>
+          <el-button @click="toMain">返回主页</el-button>
         </el-col>
       </el-row>
 
@@ -33,11 +33,13 @@
             </el-select>
           </el-col>
           <el-col :xl="{span:3}" :lg="{span:4}">
-            <el-button type="primary" style="font-weight: bolder; margin-left: -16px">搜 索 <i class="el-icon-search"></i>
+            <el-button type="primary" style="font-weight: bolder; margin-left: -16px" @click="getCodeList">搜 索 <i
+              class="el-icon-search"></i>
             </el-button>
           </el-col>
           <el-col :lg="{span:3}" :xl="{span:3}" style="margin-left: -28px">
-            <el-button type="primary" style="font-weight: bolder;" @click="addNewCode(cudType='CREATE')">新 增 <i class="el-icon-edit"></i></el-button>
+            <el-button type="primary" style="font-weight: bolder;" @click="forwardCud(cudType='CREATE')">新 增 <i
+              class="el-icon-edit"></i></el-button>
           </el-col>
         </el-row>
       </div>
@@ -56,7 +58,8 @@
           <template slot-scope="scope">
             <el-row>
               <el-col>
-                <mu-button small color="info" style="font-weight: bolder">查看详情</mu-button>
+                <mu-button small color="info" style="font-weight: bolder" @click="toCodeDetail(scope.row.codeId)">查看详情
+                </mu-button>
               </el-col>
             </el-row>
           </template>
@@ -65,12 +68,12 @@
           <template slot-scope="scope">
             <el-row :gutter="20" style="margin-left: auto;margin-right: auto;width:80%">
               <el-col :span="12">
-                <mu-button small color="primary">编辑
+                <mu-button small color="primary" @click="forwardCud('UPDATE',scope.row)">编辑
                   <mu-icon value="edit" right></mu-icon>
                 </mu-button>
               </el-col>
               <el-col :span="10">
-                <mu-button small color="error">删除
+                <mu-button small color="error" @click="forwardCud(cudType = 'DELETE',row = scope.row)">删除
                   <mu-icon value="delete" right></mu-icon>
                 </mu-button>
               </el-col>
@@ -85,7 +88,7 @@
       :visible.sync="configurationInfo.dialogIsShow"
       width="30%"
       :before-close="handleClose">
-      <el-form :model="codeForm" :label-position="configurationInfo.labelPosition" ref="codeForm" :label-width="100">
+      <el-form :model="codeForm" :label-position="configurationInfo.labelPosition" ref="codeForm" label-width="100">
         <el-form-item label="源码名称:">
           <el-input v-model="codeForm.codeName" style="width: 70%" placeholder="请输入源码名称"></el-input>
         </el-form-item>
@@ -103,12 +106,13 @@
           </el-select>
         </el-form-item>
         <el-form-item label="源码备注:">
-          <el-input type="textarea" maxlength="30" show-word-limit v-model="codeForm.codeRemark" style="width: 70%" placeholder="请输入源码名称"></el-input>
+          <el-input type="textarea" maxlength="30" show-word-limit v-model="codeForm.codeRemark" style="width: 70%"
+                    placeholder="请输入源码名称"></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
     <el-button @click="configurationInfo.dialogIsShow = false">取 消</el-button>
-    <el-button type="primary" @click="configurationInfo.dialogIsShow = false">确 定</el-button></span>
+    <el-button type="primary" @click="cudCode">确 定</el-button></span>
     </el-dialog>
   </el-container>
 </template>
@@ -119,25 +123,28 @@
     name: "ProjectInfo",
     data() {
       return {
+        // 基本配置
         configurationInfo: {
-          dialogIsShow: true,
+          dialogIsShow: false,
           labelPosition: "left",
         },
-        dialog:{
-          title:"",
-          cudType:""
+
+        //弹出框配置
+        dialog: {
+          title: "",
+          cudType: ""
         },
+
+        //项目基本信息
         projectBase: {
           projectId: "",
           projectName: "测试"
         },
-        codeList: [
-          {
-            codeId: "123",
-            codeName: "测试",
-            codeType: "CLASS"
-          }
-        ],
+
+        // 源码列表信息
+        codeList: [],
+
+        // 源码基础信息
         baseControl: {
           codeName: "",
           codeType: "",
@@ -164,55 +171,109 @@
             },
           ]
         },
-        codeForm:{
-          codeName:"",
-          codeType:"",
-          codeRemark:""
+
+        // 源码form表单
+        codeForm: {
+          codeName: "",
+          codeType: "",
+          codeRemark: "",
+          cudType: "",
+          fromProjectId: ""
+        },
+
+        // 源码查询表单
+        codeSearchForm: {
+          remark: "",
+          codeType: ""
         }
       }
     },
     mounted() {
-      this.$axios({
-        url:this.Globel.requestUrl+"/project/projectQuery?projectId="+this.$route.query.projectId,
-      }).then(res =>{
-        if(res.data.success){
-          this.codeList = res.data.data.codeInfoList;
-        }else{
-          this.$message.error(res.data.msg)
-        }
-      })
+      this.getCodeList()
     },
     methods: {
 
       /**
+       * 查询源码列表
+       * */
+      getCodeList() {
+        this.$axios({
+          url: this.Globel.requestUrl + "/project/projectQuery?projectId=" + this.$route.query.projectId,
+        }).then(res => {
+          if (res.data.success) {
+            this.codeList = res.data.data.codeInfoList;
+          } else {
+            this.$message.error(res.data.msg)
+          }
+        })
+      },
+
+      /**
        * dialog关闭
        * */
-      handleClose(){
+      handleClose() {
         this.$refs["codeForm"].resetFields();
       },
 
       /**
-       * 打开新增源码的dialog
+       * 转发cud方法
        * */
-      addNewCode(){
-        this.configurationInfo.dialogIsShow = true;
-        this.dialog.title = "新增源码"
-        this.dialog.cudType = "CREATE"
+      forwardCud(cudType, row) {
+        this.dialog.cudType = cudType;
+        this.codeForm.cudType = cudType;
+        if (cudType == "CREATE") {
+          this.configurationInfo.dialogIsShow = true;
+          this.dialog.title = "新增源码";
+          this.codeForm.fromProjectId = this.$route.query.projectId
+        }
+        if (cudType == "DELETE") {
+          this.codeForm = row;
+          this.codeForm.cudType = "DELETE";
+          this.cudCode()
+        }
+        if (cudType == "UPDATE") {
+          this.codeForm = Object.assign({}, row);
+          this.codeForm.cudType = "UPDATE";
+          this.configurationInfo.dialogIsShow = true;
+          this.dialog.title = "编辑源码";
+        }
       },
 
       /**
        * 确认新增源码
        * */
-      cudCode(){
+      cudCode() {
         this.$axios({
-          url:this.Globel.requestUrl+"/code/codeCUD",
-          data:this.codeForm,
-          method:"POST"
-        }).then(res =>{
-          if(res.data.success){
-            this.$message.success("操作成功")
+          url: this.Globel.requestUrl + "/code/codeCUD",
+          data: this.codeForm,
+          method: "POST"
+        }).then(res => {
+          if (res.data.success) {
+            this.$message.success("操作成功");
+            this.getCodeList();
+            this.configurationInfo.dialogIsShow = false;
+          } else {
+            this.$message.error(res.data.msg);
           }
         })
+      },
+
+      /**
+       * 回到主页
+       * */
+      toMain() {
+        this.$router.push({path: "/"})
+      },
+
+      /**
+       * 源码详情
+       * */
+      toCodeDetail(codeId) {
+        this.$router.push(
+          {
+            path: "/codeInfo",
+            query: {codeId: codeId}
+          })
       }
     }
   }
