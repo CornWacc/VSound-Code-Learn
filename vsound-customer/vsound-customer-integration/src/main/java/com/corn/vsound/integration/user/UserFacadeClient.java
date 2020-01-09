32caeec4f00ec8d23a8442358f338b910440a779
CornWacc <791621554@qq.com>
2020-01-09 21:48:42 +0800

    修改

--- vsound-customer/vsound-customer-integration/src/main/java/com/corn/vsound/integration/user/UserFacadeClient.java
+++ vsound-customer/vsound-customer-integration/src/main/java/com/corn/vsound/integration/user/UserFacadeClient.java
@@ -1,11 +1,5 @@
 package com.corn.vsound.integration.user;
 
-
-import com.corn.vworld.facade.user.infoquery.UserInfoQueryOrder;
-import com.corn.vworld.facade.user.infoquery.UserInfoQueryResult;
-import com.corn.vworld.facade.user.login.UserLoginOrder;
-import com.corn.vworld.facade.user.login.UserLoginResult;
-
 /**
  * @author yyc
  * @apiNote 用户远程服务请求客户端
